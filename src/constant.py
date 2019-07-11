a65b36a55f683e93a97052a5f073d62e896b030a
Mahendra Sharma <sharma.mahendra@bcg.com>
2019-07-11 12:29:22 +0530

    changes for ci/cd

--- src/constant.py
+++ src/constant.py
@@ -2,31 +2,3 @@ api_url_user_prefix = '/api/v1/user'
 api_url_user_list = "/list"
 api_url_user = "/<int:user_id>"
 api_url_user_register = "/register"
-
-
-def test(**kwargs):
-    for key, value in kwargs.items():
-        print("key={}--value={}".format(key, value))
-
-
-data = {
-    "one": "1",
-    "two": "2",
-    "three": "3"
-}
-
-import os
-
-# print(__dict__)
-# print(os.path.abspath(__file__))
-# print(data.items())
-# for key, value in data.items():
-    # print(key+"---"+value)
-# test(**data)
-
-
-def hi(*asdf,**sdf):
-    print(type(asdf))
-    print(type(sdf))
-
-hi(1,2,3,a=1,b=2)
